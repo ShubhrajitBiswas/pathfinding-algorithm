743737873fb020495076c902219af80d0df5f174
ShubhrajitBiswas <115009150+ShubhrajitBiswas@users.noreply.github.com>
2024-11-13 22:08:05 +0530

    Initial commit

---
+++ src/algorithms/index.js
@@ -0,0 +1,27 @@
+// @flow
+
+import { DIJKSTRA, BELLMAN_FORD, A_STAR, DFS, BFS } from 'constants.js';
+import Dijkstra from './dijkstra';
+import BellmanFord from './bellmanFord';
+import AStar from './AStar';
+import Dfs from './dfs';
+import Bfs from './bfs';
+
+type PathfinderType = {
+  [key: string]:
+    | typeof Dijkstra
+    | typeof BellmanFord
+    | typeof AStar
+    | typeof Dfs
+    | typeof Bfs, // Make sure there is proper spacing
+};
+
+const Pathfinder: PathfinderType = {
+  [DIJKSTRA]: Dijkstra,
+  [BELLMAN_FORD]: BellmanFord,
+  [A_STAR]: AStar,
+  [DFS]: Dfs,
+  [BFS]: Bfs,
+};
+
+export default Pathfinder;
